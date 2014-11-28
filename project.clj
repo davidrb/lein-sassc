704314b7507c3d8e592ffee7073fa7268652330e
Edward Wible <aewible@gmail.com>
2014-11-27 22:46:40 -0500

    Initial commit ported from https://hg@bitbucket.org/apribase/lein-sassc

---
+++ project.clj
@@ -0,0 +1,13 @@
+(defproject lein-sassc "0.9.1"
+  :description "Leiningen plugin to compile SASS/SCSS files with SassC."
+  :url "http://bitbucket.org/apribase/lein-sassc"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"
+            :distribution :repo}
+  :scm {:name "hg"
+        :url "https://apribase@bitbucket.org/apribase/lein-sassc"}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [me.raynes/fs "1.4.6"]]
+  :hooks [leiningen.sassc]
+  :eval-in-leiningen true
+  :min-lein-version "2.0.0")
